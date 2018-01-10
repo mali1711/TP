7c699abc150c49302d3324fb2e7fcd2173c14eae
mali <mali1711@163.com>
2018-01-10 15:26:29 +0800

    '分享练级加载'

--- Application/Users/Controller/ShareController.class.php
+++ Application/Users/Controller/ShareController.class.php
@@ -16,8 +16,14 @@ class ShareController extends Controller {
         $list['share_desc'] = $share->find($id)['share_desc'];
         $list['share_desc'] = html_entity_decode($list['share_desc']);
         $list['userid'] = $_SESSION['user']['userinfo']['users_id'];
+        $list['url'] =  'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'/users_id/'.$list['userid'];//定义访问的url
         $this->assign('list',$list);
         $this->display('Index/showShareDetail');
     }
+
+    /*
+     *用户通过连接，进入分享内容
+     * */
+
 
 }
