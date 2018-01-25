439f3d07bce39314a669f80d8fac3001fb8e5298
mali_server <mali1711@163.com>
2018-01-25 10:26:54 +0800

    数据库密码已经改了

--- Application/WX/Conf/config.php
+++ Application/WX/Conf/config.php
@@ -5,7 +5,7 @@ return array(
 	'DB_HOST'   => 'localhost', // 服务器地址
 	'DB_NAME'   => 'huike', // 数据库名
 	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => '123456', // 密码
+	'DB_PWD'    => 'Abc123456789@', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
