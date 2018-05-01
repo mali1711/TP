d536f48a7307bedf5bd81bec089a7b783d98d1e2
mali <mali1711@163.com>
2018-05-01 10:24:28 +0800

    'xx'

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,10 +2,12 @@
 return array(
     //'配置项'=>'配置值'
 
-/*    // 支付配置文件
-    'PAPMENT'   =>	array(
-        'operator_id'=>'6dbf7c7a5c1c7286dd774226caf7be7b',//收银员id
-        'app'	=>	'M178',					//测试用的app
-        'key'	=>	'3acdf926bfbcea5ad55c8682f0d2fd1d',	//测试app对应的加密key
-    ),*/
+    /*    // 支付配置文件
+        'PAPMENT'   =>	array(
+            'operator_id'=>'6dbf7c7a5c1c7286dd774226caf7be7b',//收银员id
+            'app'	=>	'M178',					//测试用的app
+            'key'	=>	'3acdf926bfbcea5ad55c8682f0d2fd1d',	//测试app对应的加密key
+        ),*/
+    'DEFAULT_MODULE' => 'Users',
+    'MODULE_ALLOW_LIST'=>array('Home','Admin','Users'),
 );
