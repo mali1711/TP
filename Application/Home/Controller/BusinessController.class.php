570d96fd10e9bf69c37289a2ec5389f40fdbbbd0
203.195.154.131 <203.195.154.131@gmail.com>
2018-07-14 22:08:30 +0800

    xas

--- Application/Home/Controller/BusinessController.class.php
+++ Application/Home/Controller/BusinessController.class.php
@@ -19,7 +19,7 @@ class BusinessController extends Controller {
     {
         $business = M('business'); // 实例化User对象$User = M('User'); // 实例化User对象
         $count      = $business->count();// 查询满足要求的总记录数
-        $Page       = new \Think\Page($count,15);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $Page       = new \Think\Page($count,40);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show       = $Page->show();// 分页显示输出
         $list = $business->limit($Page->firstRow.','.$Page->listRows)->select();
         $this->assign('list',$list);// 赋值数据集
