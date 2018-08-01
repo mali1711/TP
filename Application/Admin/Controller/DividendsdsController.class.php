4903b702de0c3152a00ac20d58813d1a74275d08
mali <mali1711@163.com>
2018-08-01 23:11:00 +0800

    'xiangqing写完'

--- Application/Admin/Controller/DividendsdsController.class.php
+++ Application/Admin/Controller/DividendsdsController.class.php
@@ -12,6 +12,47 @@ class DividendsdsController extends CommonController {
     public function __construct()
     {
         $this->business_id = $_SESSION['Admin']['business_id'];
+        parent::__construct();
+    }
+
+    /*
+     * 商家兑换页
+     * */
+    public function jiaoyi()
+    {
+        $this->display('Phone/jiaoyi');
+    }
+
+    /**
+     *
+     * 兑换详情（日，年，月账单）
+     */
+    public function duihuan()
+    {
+        $this->display('Phone/duihuan');
+    }
+
+    /**
+     *兑换详情
+     *
+     **/
+    public function xiangqing()
+    {
+        $today=strtotime(date('Y-m-d 00:00:00'));
+        $where['consume_time'] = array('egt',$today);
+        $where['business_id'] = $this->business_id;
+        $count = M('consume_list')->distinct(true)->field('users_id')->where($where)->count();
+        $count = ceil($count/3);
+        $this->assign('count',$count); 
+        $this->display('Phone/xiangqing');
+    }
+
+    /**
+     * 测试
+     */
+    public function demo()
+    {
+        $this->display('Phone/demo');
     }
 
     /**
@@ -26,19 +67,49 @@ class DividendsdsController extends CommonController {
     }
 
     /**
-     * 商家目前已经分配的红包总数
+     * 商家所有
+     * 商家目前已经分配的红包总数红包详情
      * 目前已经兑换的红包总数
      * 小数点取到后两位
      */
     public function allNum()
     {
         $where['business_id'] = $this->business_id;
-        $data['Mysum'] = floor(M('users_integral_list')->where($where)->sum('users_get_integral')*100)/100;//本商家生成的所有的分红
-        $data['allSum'] = floor(M('users_integral_list')->sum('users_get_integral')*100)/100;//整个汇客系统产生的所有的分红数量
+        $data['allSum'] = floor(M('users_integral')->sum('users_integral_num')*100)/100;//整个汇客系统产生的所有的分红数量
         $data['useSum'] = M('consume_list')->where($where)->sum('consume_list_use_integral');//本商家已经兑换的积分
+        $data['Mysum'] = $data['useSum']+floor(M('users_integral')->where($where)->sum('users_integral_num')*100)/100;//本商家生成的所有的分红
+        $TodayIncome = $this->__TodayIncome();
+        $data['TodayIncome'] = $TodayIncome['cash']+$TodayIncome['RedEn'];//今日收入（红包+现金）
         echo json_encode($data);
     }
 
+
+    /**
+     * 今日现金数和红包数
+     * 今日总的消费详情
+     * @return ajax
+     * */
+    public function DetOFCon()
+    {
+        $TodayIncome =  $this->__TodayIncome();
+        echo json_encode($TodayIncome);
+    }
+    
+    /**
+     * 今日收入
+     * @return $data['cash'] 现金
+     * @return $data['RedEn'] 红包
+     * */
+    private function __TodayIncome()
+    {
+        $today=strtotime(date('Y-m-d 00:00:00'));
+        $where['consume_time'] = array('egt',$today);
+        $where['business_id'] = $this->business_id;
+        $data['cash'] = M('consume_list')->where($where)->sum('consume_money');//当日商家已经被兑换的积分
+        $data['RedEn'] = M('consume_list')->where($where)->sum('consume_list_use_integral');//会员使用的红包数
+        return $data;
+    }
+    
     /**
      * 按天、月、年查询订单
      *
@@ -100,12 +171,35 @@ class DividendsdsController extends CommonController {
             ->where($where)->order('consume_time desc')->select();
         foreach ($userList as $k=>$v) {
             $where['users_id'] = $v['users_id'];
+            echo $v['users_id'];
+            echo '<br/>';
             $data['userExchange'][$k]['consume_list_use_integral'] = M('consume_list')->where($where)->sum('consume_list_use_integral');
             $data['userExchange'][$k]['users_name'] = M('users')->find($v['users_id'])['users_name'];
             $data['userExchange'][$k]['users_id'] = M('users')->find($v['users_id'])['users_id'];
         }
-        dump($data);
         echo json_encode($data);
+    }
+
+    /*
+     * 当日红包使用详情
+     * */
+    public function xiangQingData()
+    {
+
+        $page = $_GET['page']?$_GET['page']:1;
+        $sta = ($page-1)*3;
+        $today=strtotime(date('Y-m-d 00:00:00'));
+        $where['consume_time'] = array('egt',$today);
+        $where['business_id'] = $this->business_id;
+        $userList = M('consume_list')->distinct(true)->field('users_id')->where($where)->limit($sta,3)->select();    $xiangQingData = array();
+        foreach ($userList as $k=>$v) {
+            $where['users_id'] = $v['users_id'];
+            $xiangQingData[$k]['consume_list_use_integral'] = M('consume_list')->where($where)->sum('consume_list_use_integral');
+            $xiangQingData[$k]['users_name'] = M('users')->find($v['users_id'])['users_phone'];
+            $xiangQingData[$k]['users_id'] = M('users')->find($v['users_id'])['users_id'];
+        }
+//        dump($json);
+        echo json_encode($xiangQingData);
     }
 
 }
